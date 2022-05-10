614f7d79fd526e61ccc0ade70cbd49aaa9751580
AndrewKondra <101669003+AndrewKondra@users.noreply.github.com>
2022-05-10 14:51:12 -0400

    update

---
+++ callback/badcode.js
@@ -0,0 +1,31 @@
+function sayHello(numb, callback) {
+    let  hiThere = 'Hello ' + numb + ' times';
+   console.log
+ }
+
+function sayHello(numb) {
+    console.log(numb);
+    let greeting = numb;
+    console.log(greeting);
+    // return greeting;
+}
+function numClicks(callback) {
+    let numb = 5;
+    callback(numb);
+
+}
+numClicks(sayHello);
+;
+*/
+
+/*function sumNumbers(x, y, multNumbers) {
+    let a = multNumbers + (x + y);
+    return a;
+}
+function multNumbers(m, n) {
+    let b = m * n;
+    return b;
+}
+let answer = sumNumbers(2, 3, multNumbers(5, 7));
+console.log(answer);
+*/

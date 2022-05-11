fc865109e240bc54a52fd2302e0d4d652d5af844
AndrewKondra <101669003+AndrewKondra@users.noreply.github.com>
2022-05-11 12:11:49 -0600

    update rainfall

---
+++ callback/callback2.js
@@ -0,0 +1,20 @@
+/*function sayHello(numb) {
+    let  hiThere = 'Hello ' + numb + ' times';
+   console.log
+ }
+*/
+let x = 1;
+function sayHello(numb) {
+    console.log('hi');
+    //let greeting = numb;
+    //console.log(greeting);
+    // return greeting;
+}
+function numClicks() {
+    let y = x++
+    console.log(y);
+    return y;
+
+}
+
+//console.log(sayHello(numClicks));
